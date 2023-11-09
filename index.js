d65cf367251462bfaf271e62d20bed7c9baa908d
suzianefonseca <138014152+suzianefonseca@users.noreply.github.com>
2023-11-09 14:13:36 +0000

    outras alteraçoes

--- index.js
+++ index.js
@@ -4,17 +4,37 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.get("/oi", function ( req, res){
+app.get("/oi", function ( req, res) {
   res.send("olá mundo!")
 
 })
 
+
 const lista = ["rick sanchez", "morty smith", "sumer smith"]
+
+//read all -[GET] /item
 
 app.get("/item", function (req, res) {
   res.send(lista)
 })
 
+// read by ID[GET] /item/1:id
+
+app.get("/item/:id", function (req, res) {
+
+// pegamos o ID de rora e subtraimos 1 para ficar
+// equivalente ao indice da listaque começa em zero
+
+  const id = req.params.id - 1
+
+//acessamos o item na lista usando o indice corrigido
+ const item = lista[id]
+
+ //enviamos o item como resposta
+
+  res.send(item)
+  
+})
 
 app.listen(3000)
 
